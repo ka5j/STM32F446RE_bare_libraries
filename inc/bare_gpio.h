6e7557dddceb06c438679fc331a0c9311ea983c1
Krish <krishahuja55@gmail.com>
2025-04-28 17:22:23 -0400

    Added function needed for bare_gpio.c in header file and created enums for the choices

--- inc/bare_gpio.h
+++ inc/bare_gpio.h
@@ -13,35 +13,41 @@
 #include "device_registers.h"
 #include <stdint.h>
 
-#define GPIO_PORTA 0x00
-#define GPIO_PORTB 0x01
-#define GPIO_PORTC 0x02
-#define GPIO_PORTD 0x04
-#define GPIO_PORTE 0x05
-#define GPIO_PORTF 0x06
-#define GPIO_PORTG 0x07
-#define GPIO_PORTH 0x08
-
-#define GPIO_MODE_INPUT 0x00
-#define GPIO_MODE_OUTPUT 0x01
-#define GPIO_MODE_AF 0x02
-#define GPIO_MODE_ANALOG 0x03
-
 #define GPIO_PIN_HIGH 0x01
 #define GPIO_PIN_LOW 0x00
 
-#define GPIO_PUSH_PULL 0x00
-#define GPIO_OPEN_DRAIN 0x01
+typedef enum{
+    GPIO_MODE_INPUT = 0x00,
+    GPIO_MODE_OUTPUT = 0x01,
+    GPIO_MODE_AF = 0x02,
+    GPIO_MODE_ANALOG = 0x03
+}GPIO_Mode_t;
 
-#define GPIO_LOW_SPEED 0x00
-#define GPIO_MEDIUM_SPEED 0x01
-#define GPIO_FAST_SPEED 0x02
-#define GPIO_HIGH_SPEED 0x03
+typedef enum {
+    GPIO_OTYPE_PP = 0x00U, // Push-Pull
+    GPIO_OTYPE_OD = 0x01U  // Open-Drain
+}GPIO_OType_t;
 
-#define GPIO_FLOATING 0x00
-#define GPIO_PULL_UP 0x01
-#define GPIO_PULL_DOWN 0x02
+typedef enum {
+    GPIO_SPEED_LOW    = 0x00,
+    GPIO_SPEED_MEDIUM = 0x01,
+    GPIO_SPEED_FAST   = 0x02,
+    GPIO_SPEED_HIGH   = 0x03
+}GPIO_Speed_t;
 
+typedef enum {
+    GPIO_NOPULL = 0x00U,
+    GPIO_PULLUP = 0x01U,
+    GPIO_PULLDOWN = 0x02U
+}GPIO_Pull_t;
 
+void bare_gpio_init(GPIO_TypeDef *GPIOx, uint8_t pin, GPIO_Mode_t mode,
+    GPIO_OType_t otype, GPIO_Speed_t speed, GPIO_Pull_t pull);
+
+void bare_gpio_write(GPIO_TypeDef *GPIOx, uint8_t pin, uint8_t state);
+
+uint8_t bare_gpio_read(GPIO_TypeDef *GPIOx, uint8_t pin);
+
+void bare_gpio_toggle(GPIO_TypeDef *GPIOx, uint8_t pin);
 
 #endif /* GPIO_H_ */
