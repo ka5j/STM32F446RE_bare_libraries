763f820d52408eed860943e8d5bb5d71216fa2c6
Krish <krishahuja55@gmail.com>
2025-04-28 19:14:47 -0400

    create an RCC struct with all registers

--- inc/device_registers.h
+++ inc/device_registers.h
@@ -36,6 +36,51 @@
 #define RCC_BASE            (AHB1PERIPH_BASE + 0x3800U)
 
 /*******************************************************************************************
+ * RCC Peripheral Definitions
+ *******************************************************************************************/
+typedef struct
+{
+    volatile uint32_t CR;             /*!< RCC clock control register,                         Address offset: 0x00 */
+    volatile uint32_t PLLCFGR;        /*!< RCC PLL configuration register,                     Address offset: 0x04 */
+    volatile uint32_t CFGR;           /*!< RCC clock configuration register,                   Address offset: 0x08 */
+    volatile uint32_t CIR;            /*!< RCC clock interrupt register,                       Address offset: 0x0C */
+    volatile uint32_t AHB1RSTR;       /*!< RCC AHB1 peripheral reset register,                  Address offset: 0x10 */
+    volatile uint32_t AHB2RSTR;       /*!< RCC AHB2 peripheral reset register,                  Address offset: 0x14 */
+    volatile uint32_t AHB3RSTR;       /*!< RCC AHB3 peripheral reset register,                  Address offset: 0x18 */
+    uint32_t RESERVED0;               /*!< Reserved,                                            Address offset: 0x1C */
+    volatile uint32_t APB1RSTR;       /*!< RCC APB1 peripheral reset register,                  Address offset: 0x20 */
+    volatile uint32_t APB2RSTR;       /*!< RCC APB2 peripheral reset register,                  Address offset: 0x24 */
+    uint32_t RESERVED1;               /*!< Reserved,                                            Address offset: 0x28 */
+    uint32_t RESERVED2;               /*!< Reserved,                                            Address offset: 0x2C */
+    volatile uint32_t AHB1ENR;        /*!< RCC AHB1 peripheral clock enable register,           Address offset: 0x30 */
+    volatile uint32_t AHB2ENR;        /*!< RCC AHB2 peripheral clock enable register,           Address offset: 0x34 */
+    volatile uint32_t AHB3ENR;        /*!< RCC AHB3 peripheral clock enable register,           Address offset: 0x38 */
+    uint32_t RESERVED3;               /*!< Reserved,                                            Address offset: 0x3C */
+    volatile uint32_t APB1ENR;        /*!< RCC APB1 peripheral clock enable register,           Address offset: 0x40 */
+    volatile uint32_t APB2ENR;        /*!< RCC APB2 peripheral clock enable register,           Address offset: 0x44 */
+    uint32_t RESERVED4;               /*!< Reserved,                                            Address offset: 0x48 */
+    uint32_t RESERVED5;               /*!< Reserved,                                            Address offset: 0x4C */
+    volatile uint32_t AHB1LPENR;      /*!< RCC AHB1 peripheral clock enable in low power mode,  Address offset: 0x50 */
+    volatile uint32_t AHB2LPENR;      /*!< RCC AHB2 peripheral clock enable in low power mode,  Address offset: 0x54 */
+    volatile uint32_t AHB3LPENR;      /*!< RCC AHB3 peripheral clock enable in low power mode,  Address offset: 0x58 */
+    uint32_t RESERVED6;               /*!< Reserved,                                            Address offset: 0x5C */
+    volatile uint32_t APB1LPENR;      /*!< RCC APB1 peripheral clock enable in low power mode,  Address offset: 0x60 */
+    volatile uint32_t APB2LPENR;      /*!< RCC APB2 peripheral clock enable in low power mode,  Address offset: 0x64 */
+    uint32_t RESERVED7;               /*!< Reserved,                                            Address offset: 0x68 */
+    uint32_t RESERVED8;               /*!< Reserved,                                            Address offset: 0x6C */
+    volatile uint32_t BDCR;           /*!< RCC Backup domain control register,                  Address offset: 0x70 */
+    volatile uint32_t CSR;            /*!< RCC clock control & status register,                 Address offset: 0x74 */
+    uint32_t RESERVED9;               /*!< Reserved,                                            Address offset: 0x78 */
+    uint32_t RESERVED10;              /*!< Reserved,                                            Address offset: 0x7C */
+    volatile uint32_t SSCGR;          /*!< RCC spread spectrum clock generation register,       Address offset: 0x80 */
+    volatile uint32_t PLLI2SCFGR;     /*!< RCC PLLI2S configuration register,                   Address offset: 0x84 */
+    volatile uint32_t PLLSAICFGR;     /*!< RCC PLLSAI configuration register,                   Address offset: 0x88 */
+    volatile uint32_t DCKCFGR;        /*!< RCC dedicated clocks configuration register,         Address offset: 0x8C */
+    volatile uint32_t CKGATENR;       /*!< RCC clocks gated enable register,                    Address offset: 0x90 */
+    volatile uint32_t DCKCFGR2;       /*!< RCC dedicated clocks configuration register 2,       Address offset: 0x94 */
+} RCC_TypeDef;
+
+/*******************************************************************************************
  * APB2 Peripherals
  *******************************************************************************************/
 #define EXTI_BASE           (APB2PERIPH_BASE + 0x3C00U)
