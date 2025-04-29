0a2b7b7d10a6fd4230332cc667ac57cedfa50e6b
Krish <krishahuja55@gmail.com>
2025-04-29 16:31:06 -0400

    Added enums for systick

--- inc/bare_systick.h
+++ inc/bare_systick.h
@@ -7,8 +7,18 @@
 #define SYSTICK_TICKS 16000000 // 1 second timer
 
 typedef enum{
-    EXTERNAL_CLK = 0x00U,
-    PROCESSOR_CLK = 0x01U
+    SYSTICK_EXTERNAL_CLK = 0x00U,
+    SYSTICK_PROCESSOR_CLK = 0x01U
 }SysTick_Clk_t;
+
+typedef enum{
+    SYSTICK_DISABLE_INTERRUPT = 0x00U,
+    SYSTICK_ENABLE_INTERRUPT = 0x01U
+}SysTick_Interrupt_t;
+
+typedef enum{
+    SYSTICK_DISABLE = 0x00U,
+    SYSTICK_ENABLE = 0x01U
+}SysTick_Start_t;
 
 #endif /* BARE_SYSTICK_H_ */
